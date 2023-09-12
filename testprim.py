220e226a17325312695a396c30ca28d62a312d11
Jose Alvarez <oxenjose91@hotmail.com>
2023-09-12 20:12:11 +0200

    kod.py.primtalen
    Signed-off-by: Jose Alvarez <oxenjose91@hotmail.com>

---
+++ testprim.py
@@ -0,0 +1,10 @@
+def primtal(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+prim = [x for x in range(100) if primtal(x)]
+print(f"primtalen är: {prim}")
